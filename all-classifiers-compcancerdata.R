d3ba28604f0f3a1198b3aed1ab8714f8ebcdc0a7
Aytijhya <aytijhyasaha02@gmail.com>
2022-07-21 23:07:59 +0530

    ,,

--- all-classifiers-compcancerdata.R
+++ all-classifiers-compcancerdata.R
@@ -21,9 +21,25 @@ labels.rename <- function(X){
 
 X=list()
 X[[1]]=armstrong.2002.v1_database
+X[[2]]=chen.2002_database
+X[[3]]=chowdary.2006_database
+X[[4]]=golub.1999.v1_database
+X[[5]]=gordon.2002_database
+X[[6]]=laiho.2007_database
+X[[7]]=nutt.2003.v2_database
+X[[8]]=nutt.2003.v2_database
+X[[9]]=pomeroy.2002.v1_database
+X[[10]]=shipp.2002.v1_database
+X[[11]]=singh.2002_database
+X[[12]]=west.2001_database
+X[[13]]=yeoh.2002.v1_database
 
-X[[1]]=t(as.matrix(X[[1]]))[-1,]
-X[[1]]=cbind(X[[1]],X[[1]][,2]) %>% labels.rename()
-write_xlsx(X[[1]] %>% as.data.frame(), 
-              path = paste0("/Users/aytijhyasaha/Desktop/projects/Classification-Summer-2022/","Dataset",1,".xlsx"))
+filenames=gsub('txt','xlsx',filenames)
+for(i in 1:13){
+   X[[i]]=t(as.matrix(X[[i]]))[-1,]%>% labels.rename()
+   write_xlsx(X[[i]] %>% as.data.frame(), 
+              path = paste0("/Users/aytijhyasaha/Desktop/projects/Classification-Summer-2022/Compcancer-datasets/",filenames[i]))
+   
+}
+
 

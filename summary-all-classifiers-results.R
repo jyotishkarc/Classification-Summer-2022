ac29ec330177875b4e5f1f26168c2a34f5063090
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2022-07-28 14:34:11 +0530

    Update summary-all-classifiers-results.R

--- summary-all-classifiers-results.R
+++ summary-all-classifiers-results.R
@@ -11,7 +11,11 @@ results.UCR <- list.files(path.UCR)
 results.CompCancer <- list.files(path.CompCancer)
 
 
-
+A <- sapply(1:length(results.UCR), function(val){
+  
+  temp.file <- read_excel(paste0(path.UCR, results.UCR[val]))
+  return(temp.file[c(52,53),])
+})
 
 
 

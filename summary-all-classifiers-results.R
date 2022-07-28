ccc1ccb9436aabf3a9f3de1135b2b2dbf6f54c2b
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2022-07-28 14:23:30 +0530

    Initial commit

---
+++ summary-all-classifiers-results.R
@@ -0,0 +1,17 @@
+
+
+path <- "C:/Users/JYOTISHKA/Desktop/"
+
+path.simulated <- paste0(path, "all-classifiers-TwoClass-simulated-new/")
+path.UCR <- paste0(path, "UCR-TwoClass-Results/")
+path.CompCancer <- paste0(path, "CompCancer-TwoClass-Results/")
+
+results.simulated <- list.files(path.simulated)
+results.UCR <- list.files(path.UCR)
+results.CompCancer <- list.files(path.CompCancer)
+
+
+
+
+
+

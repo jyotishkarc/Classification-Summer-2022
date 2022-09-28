b0b304bcc8790c80da9d34cb866cc9e304882505
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2022-09-28 06:39:30 +0530

    ...

--- our-classifiers-UCR-MultiClass-JRC.R
+++ our-classifiers-UCR-MultiClass-JRC.R
@@ -40,12 +40,9 @@ rho <- function(a,b,c){
 
 ################################################### Classifier Function for Test Observations
 
-classify.parallel <- function(Z, X, Y, #B,
+classify.parallel <- function(Z, X, Y,
                               T.FF, T.GG, T.FG,
-                              # T.FF.boot, T.GG.boot, T.FG.boot,
-                              W, #W.boot,
-                              S_FG, #S_FG.boot
-                              ){
+                              W, S_FG){
    # print("Classification starting")
    R <- nrow(Z)
    Q <- rbind(X,Y)
@@ -76,24 +73,6 @@ classify.parallel <- function(Z, X, Y, #B,
       rowMeans() / (n+m)
    
    
-   # ##### T_FZ.rho.boot
-   # T_FZ.rho.boot <- function(vec){
-   #    i <- vec[1]
-   #    j <- vec[2]
-   #    
-   #    return(sum(sapply(1:M,function(val){
-   #       rho(Z[i,],X[j,],B[val,])
-   #    })))
-   # }
-   # 
-   # index.mat <- cbind(rep(1:R, each = n),rep(1:n, times = R))
-   # 
-   # T_FZ.boot <- index.mat %>%
-   #    parApply(cl, ., 1, T_FZ.rho.boot) %>%
-   #    matrix(nrow = R, ncol = n, byrow = TRUE) %>% 
-   #    rowMeans() / M
-   
-   
    ########################################### T_FZ
    
    ##### T_GZ.rho.fun
@@ -114,75 +93,33 @@ classify.parallel <- function(Z, X, Y, #B,
       rowMeans() / (n+m)
    
    
-   # ##### T_GZ.rho.boot
-   # T_GZ.rho.boot <- function(vec){
-   #    i <- vec[1]
-   #    j <- vec[2]
-   #    
-   #    return(sum(sapply(1:M,function(val){
-   #       rho(Z[i,],Y[j,],B[val,])
-   #    })))
-   # }
-   # 
-   # index.mat <- cbind(rep(1:R, each = m),rep(1:m, times = R))
-   # 
-   # T_GZ.boot <- index.mat %>%
-   #    parApply(cl, ., 1, T_GZ.rho.boot) %>%
-   #    matrix(nrow = R, ncol = m, byrow = TRUE) %>% 
-   #    rowMeans() / M
-   
-   
-   
    L_FZ <- T_FZ - rep(T.FF, R)/2
    L_GZ <- T_GZ - rep(T.GG, R)/2
    S_Z <- L_FZ + L_GZ - T.FG
    
-   # L_FZ.boot <- T_FZ.boot - rep(T.FF.boot, R)/2
-   # L_GZ.boot <- T_GZ.boot - rep(T.GG.boot, R)/2
-   # S_Z.boot <- L_FZ.boot + L_GZ.boot - T.FG.boot
-   
    W0_FG <- W[[1]]
-   # W1_FG <- W[[2]]
-   # W2_FG <- W[[3]]
    
-   # W0_FG.boot <- W.boot[[1]]
-   # # W1_FG.boot <- W.boot[[2]]
-   # # W2_FG.boot <- W.boot[[3]]
-   
-   prac.label.1 <- prac.label.2 <- prac.label.3 <- 
-      # prac.label.1.boot <- prac.label.2.boot <- prac.label.3.boot <- 
-      rep(0, R)
+   prac.label.1 <- prac.label.2 <- prac.label.3 <- rep(0, R)
    
    #### CLASSIFIER 1
    delta1_Z <- L_GZ - L_FZ
-   # delta1_Z.boot <- L_GZ.boot - L_FZ.boot
    
    prac.label.1[which(delta1_Z > 0)] <- 1
-   # prac.label.1.boot[which(delta1_Z.boot > 0)] <- 1
    prac.label.1[which(delta1_Z <= 0)] <- 2
-   # prac.label.1.boot[which(delta1_Z.boot <= 0)] <- 2
    
    #### CLASSIFIER 2
    delta2_Z <- W0_FG * delta1_Z + S_FG * S_Z
-   # delta2_Z.boot <- W0_FG.boot * delta1_Z.boot + S_FG.boot * S_Z.boot
    
    prac.label.2[which(delta2_Z > 0)] <- 1
-   # prac.label.2.boot[which(delta2_Z.boot > 0)] <- 1
    prac.label.2[which(delta2_Z <= 0)] <- 2
-   # prac.label.2.boot[which(delta2_Z.boot <= 0)] <- 2
    
    #### CLASSIFIER 3
    delta3_Z <- W0_FG * sign(delta1_Z) + S_FG * sign(S_Z)
-   # delta3_Z.boot <- W0_FG.boot * sign(delta1_Z.boot) + S_FG.boot * sign(S_Z.boot)
    
    prac.label.3[which(delta3_Z > 0)] <- 1
-   # prac.label.3.boot[which(delta3_Z.boot > 0)] <- 1
    prac.label.3[which(delta3_Z <= 0)] <- 2
-   # prac.label.3.boot[which(delta3_Z.boot <= 0)] <- 2
    
-   prac.label <- list(prac.label.1, prac.label.2, prac.label.3,
-                      # prac.label.1.boot, prac.label.2.boot, prac.label.3.boot
-                      )
+   prac.label <- list(prac.label.1, prac.label.2, prac.label.3)
    
    return(prac.label)
 }
@@ -285,206 +222,142 @@ for(h in 1:length(files.UCR)){
    
    ground.truth <- dataset[,1] %>% unlist() %>% as.numeric()
    
-   for(ind in 1:nrow(classes.mat)){
-      
-      A <- classes.mat[ind, 1] 
-      B <- classes.mat[ind, 2]
-      
-      class.A <- which(ground.truth == A)
-      class.B <- which(ground.truth == B)   
-      
-      n <- round(length(class.A) * 0.5)
-      m <- round(length(class.B) * 0.5)
-      
-      train.index <- test.index <- list()
-      
-      for(u in 1:iterations){
-         train.index[[u]] <- c(sample(class.A, n),sample(class.B, m))
-         test.index[[u]] <- setdiff(1:N, train.index[[u]])
-      }
-      
-      error.prop.1 <- error.prop.2 <- error.prop.3 <- c()
-      
-      
-      
-      
-      
-      
-   }
-   
-   
-   
-   res.list <- list()
-   
-   cat("Started - Our Classifiers:")
-   print(Sys.time())
-   
-   ############################################## Our Classifiers
    
    for(u in 1:iterations){
       
-      if(u %% 1 == 0) print(u)
-      start.time <- proc.time()[3]
       
-      X <- dataset[which(dataset[train.index[[u]],1] == 1), -1]
-      Y <- dataset[which(dataset[train.index[[u]],1] == 2), -1]
-      Q <- rbind(X,Y)
-      B <- Q[sample(1:nrow(Q), size = M, replace = TRUE), ]
+      res.list <- list()
       
-      Z <- dataset[test.index[[u]], -1]     ## Test Observations
+      # cat("Started - Our Classifiers:")
+      print(Sys.time())
       
-      # if (u %% 1 == 0) {print(u)}
-      
-      n <- nrow(X)
-      m <- nrow(Y)
-      
-      clusterExport(cl, c('X','Y','Q','B','M','n','m','rho'))
+      ############################################## Our Classifiers
       
       
-      ########################################### T.FG
-      
-      ##### T.FG.rho.fun
-      T.FG.rho.fun <- function(vec){
-         i <- vec[1]
-         j <- vec[2]
+      for(ind in 1:nrow(classes.mat)){   
+         if(u %% 1 == 0) print(u)
+         start.time <- proc.time()[3]
          
-         return(sum(sapply(1:(n+m),function(val){
-            rho(X[i,],Y[j,],Q[val,])
-         })))
-      }
-      
-      index.mat <- cbind(rep(1:n, each = m),rep(1:m, times = n))
-      
-      T.FG <- index.mat %>% 
-         parApply(cl, ., 1, T.FG.rho.fun) %>% sum() / ((n+m)*n*m)
-      
-      # ##### T.FG.rho.boot
-      # T.FG.rho.boot <- function(vec){
-      #    i <- vec[1]
-      #    j <- vec[2]
-      #    
-      #    return(sum(sapply(1:M, function(val){
-      #       rho(X[i,],Y[j,],B[val,])
-      #    })))
-      # }
-      # 
-      # index.mat <- cbind(rep(1:n, each = m),rep(1:m, times = n))
-      # 
-      # T.FG.boot <- index.mat %>% 
-      #    parApply(cl, ., 1, T.FG.rho.boot) %>% sum() / (M*n*m)
-      
-      
-      
-      ########################################### T.FF
-      
-      ##### T.FF.rho.fun
-      T.FF.rho.fun <- function(vec){
-         i <- vec[1]
-         j <- vec[2]
+         A <- classes.mat[ind, 1] 
+         B <- classes.mat[ind, 2]
          
-         return(sum(sapply(1:(n+m),function(val){
-            rho(X[i,],X[j,],Q[val,])
-         })))
-      }
-      
-      index.mat <- cbind(rep(1:n, each = n),rep(1:n, times = n))
-      
-      T.FF <- index.mat %>% 
-         parApply(cl, ., 1, T.FF.rho.fun) %>% sum() / ((n+m)*n*(n-1))
-      
-      
-      # ##### T.FF.rho.boot
-      # T.FF.rho.boot <- function(vec){
-      #    i <- vec[1]
-      #    j <- vec[2]
-      #    
-      #    return(sum(sapply(1:M, function(val){
-      #       rho(X[i,],X[j,],B[val,])
-      #    })))
-      # }
-      # 
-      # index.mat <- cbind(rep(1:n, each = n),rep(1:n, times = n))
-      # 
-      # T.FF.boot <- index.mat %>% 
-      #    parApply(cl, ., 1, T.FF.rho.boot) %>% sum() / (M*n*(n-1))
-      
-      
-      
-      ########################################### T.GG
-      
-      ##### T.GG.rho.fun
-      T.GG.rho.fun <- function(vec){
-         i <- vec[1]
-         j <- vec[2]
+         class.A <- which(ground.truth == A)
+         class.B <- which(ground.truth == B)   
          
-         return(sum(sapply(1:(n+m),function(val){
-            rho(Y[i,],Y[j,],Q[val,])
-         })))
+         n <- round(length(class.A) * 0.5)
+         m <- round(length(class.B) * 0.5)
+         
+         train.index <- test.index <- list()
+         
+         for(u in 1:iterations){
+            train.index[[u]] <- c(sample(class.A, n), sample(class.B, m))
+            test.index[[u]] <- setdiff(c(class.A, class.B), train.index[[u]])
+         }
+         
+         error.prop.1 <- error.prop.2 <- error.prop.3 <- c()
+         
+         
+         X <- dataset[which(dataset[train.index[[u]],1] == A), -1]
+         Y <- dataset[which(dataset[train.index[[u]],1] == B), -1]
+         Q <- rbind(X,Y)
+         
+         Z <- dataset[test.index[[u]], -1]     ## Test Observations
+         
+         # if (u %% 1 == 0) {print(u)}
+         
+         n <- nrow(X)
+         m <- nrow(Y)
+         
+         clusterExport(cl, c('X','Y','Q','M','n','m','rho'))
+         
+         
+         ########################################### T.FG
+         
+         ##### T.FG.rho.fun
+         T.FG.rho.fun <- function(vec){
+            i <- vec[1]
+            j <- vec[2]
+            
+            return(sum(sapply(1:(n+m),function(val){
+               rho(X[i,],Y[j,],Q[val,])
+            })))
+         }
+         
+         index.mat <- cbind(rep(1:n, each = m),rep(1:m, times = n))
+         
+         T.FG <- index.mat %>% 
+            parApply(cl, ., 1, T.FG.rho.fun) %>% sum() / ((n+m)*n*m)
+         
+         
+         ########################################### T.FF
+         
+         ##### T.FF.rho.fun
+         T.FF.rho.fun <- function(vec){
+            i <- vec[1]
+            j <- vec[2]
+            
+            return(sum(sapply(1:(n+m),function(val){
+               rho(X[i,],X[j,],Q[val,])
+            })))
+         }
+         
+         index.mat <- cbind(rep(1:n, each = n),rep(1:n, times = n))
+         
+         T.FF <- index.mat %>% 
+            parApply(cl, ., 1, T.FF.rho.fun) %>% sum() / ((n+m)*n*(n-1))
+         
+         
+         ########################################### T.GG
+         
+         ##### T.GG.rho.fun
+         T.GG.rho.fun <- function(vec){
+            i <- vec[1]
+            j <- vec[2]
+            
+            return(sum(sapply(1:(n+m),function(val){
+               rho(Y[i,],Y[j,],Q[val,])
+            })))
+         }
+         
+         index.mat <- cbind(rep(1:m, each = m),rep(1:m, times = m))
+         
+         T.GG <- index.mat %>% 
+            parApply(cl, ., 1, T.GG.rho.fun) %>% sum() / ((n+m)*m*(m-1))
+         
+         
+         ########## W
+         W0_FG <- 2 * T.FG - T.FF - T.GG
+         W1_FG <- W0_FG^2 / 2 + (T.FF - T.GG)^2 / 2
+         W2_FG <- W0_FG / 2 + abs(T.FF - T.GG) / 2
+         
+         W <- list(W0_FG, W1_FG, W2_FG)
+         S_FG <- T.FF - T.GG
+         
+         
+         ########## Test Observations
+         
+         ground.label <- ground.truth[test.index[[u]]]
+         
+         clusterExport(cl, c('Z'))
+         
+         ############################################################### Classification
+         prac.label <- classify.parallel(Z, X, Y,
+                                         T.FF, T.GG, T.FG,
+                                         W, S_FG)
+         ###############################################################
+         
+         
+         
+         
+         error.prop.1[u] <- length(which(ground.label != prac.label[[1]])) / nrow(Z)
+         error.prop.2[u] <- length(which(ground.label != prac.label[[2]])) / nrow(Z)
+         error.prop.3[u] <- length(which(ground.label != prac.label[[3]])) / nrow(Z)
       }
-      
-      index.mat <- cbind(rep(1:m, each = m),rep(1:m, times = m))
-      
-      T.GG <- index.mat %>% 
-         parApply(cl, ., 1, T.GG.rho.fun) %>% sum() / ((n+m)*m*(m-1))
-      
-      
-      # ##### T.GG.rho.boot
-      # T.GG.rho.boot <- function(vec){
-      #    i <- vec[1]
-      #    j <- vec[2]
-      #    
-      #    return(sum(sapply(1:M,function(val){
-      #       rho(Y[i,],Y[j,],B[val,])
-      #    })))
-      # }
-      # 
-      # index.mat <- cbind(rep(1:m, each = m),rep(1:m, times = m))
-      # 
-      # T.GG.boot <- index.mat %>% 
-      #    parApply(cl, ., 1, T.GG.rho.boot) %>% sum() / (M*m*(m-1))
-      
-      
-      ########## W
-      W0_FG <- 2 * T.FG - T.FF - T.GG
-      W1_FG <- W0_FG^2 / 2 + (T.FF - T.GG)^2 / 2
-      W2_FG <- W0_FG / 2 + abs(T.FF - T.GG) / 2
-      
-      W <- list(W0_FG, W1_FG, W2_FG)
-      S_FG <- T.FF - T.GG
-      
-      # ########## W.boot
-      # W0_FG.boot <- 2 * T.FG.boot - T.FF.boot - T.GG.boot
-      # W1_FG.boot <- W0_FG.boot^2 / 2 + (T.FF.boot - T.GG.boot)^2 / 2
-      # W2_FG.boot <- W0_FG.boot / 2 + abs(T.FF.boot - T.GG.boot) / 2
-      # 
-      # W.boot <- list(W0_FG.boot, W1_FG.boot, W2_FG.boot)
-      # S_FG.boot <- T.FF.boot - T.GG.boot
-      
-      
-      
-      ########## Test Observations
-      
-      ground.label <- ground.truth[test.index[[u]]]
-      
-      clusterExport(cl, c('Z'))
-      
-      ##################################################################### Classification
-      prac.label <- classify.parallel(Z, X, Y, #B,
-                                      T.FF, T.GG, T.FG,
-                                      # T.FF.boot, T.GG.boot, T.FG.boot,
-                                      W, #W.boot,
-                                      S_FG, #S_FG.boot
-                                      )
-      #####################################################################
-      
-      error.prop.1[u] <- length(which(ground.label != prac.label[[1]])) / nrow(Z)
-      error.prop.2[u] <- length(which(ground.label != prac.label[[2]])) / nrow(Z)
-      error.prop.3[u] <- length(which(ground.label != prac.label[[3]])) / nrow(Z)
-      
-      # error.prop.1.boot[u] <- length(which(ground.label != prac.label[[4]])) / nrow(Z)
-      # error.prop.2.boot[u] <- length(which(ground.label != prac.label[[5]])) / nrow(Z)
-      # error.prop.3.boot[u] <- length(which(ground.label != prac.label[[6]])) / nrow(Z)
    }
+   
+   
+   
+   
    
    cat("Started - Popular Classifiers:")
    print(Sys.time())

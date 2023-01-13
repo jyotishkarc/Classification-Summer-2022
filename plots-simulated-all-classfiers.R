4df027f3dbc180b4244791f4d57ec9249d0f1f6e
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2023-01-13 18:58:31 +0530

    Stable version

--- plots-simulated-all-classfiers.R
+++ plots-simulated-all-classfiers.R
@@ -5,10 +5,17 @@ library(ggplot2)
 
 path.our <- "C:/Users/JYOTISHKA/Desktop/delta0-TwoClass-simulated/"
 # path.our <- "G:/Projects/Prof. Subhajit Dutta (Summer, 2022)/Results/All-Latest-Results/Simulated/"
+
 files.path.our <- list.files(path.our)[1:5]
 
 path.pop <- "C:/Users/JYOTISHKA/Desktop/all-classifiers-TwoClass-simulated-new/"
 files.path.pop <- list.files(path.pop)
+
+examples <- c("Cauchy(0,1) vs Cauchy(0,2)",
+              "Cauchy(0,1) vs Cauchy(0,2)",
+              expression(paste("Normal(0,3) vs ",t[3])),
+              "Normal(1,1) vs Normal(1,2)",
+              "Lognormal(1,1) vs Lognormal(1.25,1)")
 
 plt <- df.summary.error <- df.summary.se <- list()
 
@@ -17,8 +24,7 @@ for(h in 1:5){
    df.list.our <- rio::import_list(paste0(path.our, files.path.our[h]))
    df.list.pop <- rio::import_list(paste0(path.pop, files.path.pop[h]))
    
-   ifelse(files.path.our[h] == "all-C01-vs-C02.xlsx", 
-          iterations <- 50, iterations <- 100)
+   iterations <- 100
    
    df.summary.error[[h]] <-
          df.summary.se[[h]] <- data.frame("d" = c(5,10,25,50,100,250,500,1000),
@@ -45,17 +51,50 @@ for(h in 1:5){
                                         min(pop.errors[6:13]), pop.errors[14])
       df.summary.se[[h]][i, -1] <- c(del.se, pop.se[1:5], 
                                         min(pop.se[6:13]), pop.se[14])
+      
+      df.summary.error[[h]]$d <- df.summary.se[[h]]$d <- 1:8
    }
    
-   plt[[h]] <- df.summary.error[[h]] %>%
-                  reshape2::melt(id = "d") %>% 
-                  ggplot(aes(x = d)) +
-                  geom_line(aes(y = value, color = variable), linewidth = 1)
+   ## Ex-1: C(0,1)-vs-C(0,2)
+   ## Ex-2: C(0,1)-vs-C(1,1)
+   ## Ex-3: N(0,3)-vs-t(3)
+   ## Ex-4: N(1,1)-vs-N(1,2)
+   ## Ex-5: lgn(1,1)-vs-lgn(1.25,1)
+   
+   if(TRUE){
+      plt[[h]] <- df.summary.error[[h]] %>%
+                     reshape2::melt(id = "d") %>% 
+                     cbind("se" = df.summary.se[[h]] %>% 
+                                    reshape2::melt(id = "d") %>% 
+                                    select(value) %>% 
+                                    unlist() %>% 
+                                    as.numeric()) %>%
+                     ggplot(aes(x = d)) +
+                     geom_line(aes(y = value, color = variable), linewidth = 0.9) +
+                     geom_errorbar(aes(ymin = value - se, ymax = value + se), width=0.08,
+                                 position = position_dodge(0.05)) + 
+                     scale_x_discrete(name = "Dimension",
+                                      limits = c("5","10","25","50","100",
+                                                 "250","500","1000")) +
+                     scale_color_discrete(labels = c(eval(rlang::parse_exprs("delta[0]")),
+                                                     'Bayes',
+                                                     'GLMNET','NN-RAND',
+                                                     'SVM-Linear','SVM-RBF',
+                                                     'Neural Net','1-NN')) +
+                     ggtitle(examples[h]) +
+                     theme_light() +
+                     theme(legend.title = element_blank(),
+                           plot.title = element_text(face = "bold", hjust = 0.5))
+   }
+   
+   print(h)
 }
 
 
-
-
+ggpubr::ggarrange(plt[[4]] + ylab("Misclassification Rate"),
+                  plt[[3]] + ylab(""), 
+                  ncol = 2, 
+                  common.legend = TRUE, legend = "bottom")
 
 
 
